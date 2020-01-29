0656cc465aca7e507a7a597b823070907865aa4b
Aresky Berkane <areskyberkane@gmail.com>
2020-01-29 11:34:09 +0000

    add fetch data

--- src/containers/NewsBox.js
+++ src/containers/NewsBox.js
@@ -8,9 +8,42 @@ class NewsBox extends Component{
     constructor(props) {
         super(props);
         this.state={
-
+            ids: [],
+            stories:[]
         }
     }
+
+    componentDidMount(){
+        Promise.all([this.loadIds()])
+    }
+
+    // functions*****************************
+     loadIds(){
+        const url = "https://hacker-news.firebaseio.com/v0/topstories.json";
+        fetch(url)
+        .then(res => res.json())
+        .then(ids => this.ids = ids)
+        .then(this.loadStories(100))
+    }
+
+    loadStories(amount){
+    
+        for(let i = 0; i < amount ; i++){
+            let storyId = this.ids[i];
+            fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
+            .then(res => res.json())
+            .then(story => this.stories.push(story))
+        }
+    }
+
+
+
+
+
+
+    // **************************************
+
+
     
 
     render(){
@@ -19,7 +52,7 @@ class NewsBox extends Component{
             <Header title = "True News"></Header>
             <SearchBar></SearchBar>
             <LoadAmount></LoadAmount>
-            <NewsList></NewsList>
+            <NewsList stories={this.stories}></NewsList>
             </div>
         )
     }
