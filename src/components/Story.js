25ea85ca5e3a5dd992cf3925c6d5c3c5a0bb0fa1
weinbecher <382316736@qq.com>
2020-01-29 14:28:50 +0000

    Story DONE

--- src/components/Story.js
+++ src/components/Story.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 
 const Story = (props) => {
 
+    console.log(props.url)
     return(
-    <h4>{props.title}</h4>
+
+        <h4>  <a href = {props.url} target = "_blank">{props.title}  </a></h4>
+  
     )
 }
 
