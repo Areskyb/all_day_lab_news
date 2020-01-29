b5b2c1e70929693e0cee6e6fef8ca51b77029a1b
weinbecher <382316736@qq.com>
2020-01-29 14:28:38 +0000

    searchBarDone

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
 const SearchBar = (props) => {
+    function handleStorySearch(event){
+        props.onSearch(event.target.value)
+    }
 
     return(
         <div className="search-bar">
-        <label>Search for  news: </label>
-        <input type="text"></input>
+        <label>Search for news: </label>
+        <input type="text" onChange={handleStorySearch}></input>
         </div>
     )
 }
