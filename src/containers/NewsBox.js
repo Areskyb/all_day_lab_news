76ff15ead7a136c9ea4ccbe0e4063dccfedaa43c
Aresky Berkane <areskyberkane@gmail.com>
2020-01-29 10:40:19 +0000

    news skeleton

---
+++ src/containers/NewsBox.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class NewsBox extends Component{
+    constructor(props) {
+        super(props);
+        this.state={
+
+        }
+    }
+    
+
+    render(){
+        return(
+            <h1>Hello ooooo o o o o</h1>
+        )
+    }
+}
+
+export default NewsBox;
